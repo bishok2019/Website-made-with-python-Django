c610674c6b6a15cda5cecbc4a503373b5ece2537
Bishok Paudel <bishokpaudel57@gmail.com>
2023-12-31 18:59:23 +0545

    registration system was enabled
    with sign in , sign out and sign up functionality

--- Hello/home/urls.py
+++ Hello/home/urls.py
@@ -26,6 +26,9 @@ urlpatterns = [
     path("about", views.about, name='about'),
     path("service", views.service, name='service'),
     path("contact", views.contact, name='contact'),
+    path("signup", views.signup, name='signup'),
+    path("signin", views.signin, name='signin'),
+    path("signout", views.signout, name='signout')
 
 ]
 if settings.DEBUG:
--- Hello/home/views.py
+++ Hello/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
 from datetime import datetime
 from home.models import Contact
 from home.models import Video
@@ -6,25 +6,61 @@ from home.models import Like
 from home.models import Dislike
 from home.models import Comment
 from django.contrib import messages
+from django.contrib.auth.models import User
 from .models import Video
 from .models import News  # Import the News model
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
+def signup(request):
+    if request.method == "POST":
+        # username = request.POST.get('username')
+        username = request.POST['username']
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        myuser = User.objects.create_user(username=username, password=password)
+        myuser.first_name = firstname
+        myuser.last_name = lastname
+        myuser.email = email
+        myuser.save()
+
+        messages.success(request, "Registration Success!")
+        return redirect(signin)
+    # message={"hello":"welcome"}
+    return render(request, "signup.html")
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate (username = username , password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged in successfully!")
+            request.session.save()
+            return render(request,"index.html",{'username': username})
+        else:
+            messages.error(request,"Bad request")
+            return redirect(signin)
+    return render(request, "signin.html")
+
+
 def index(request):
-    
-    videos = Video.objects.all()
     my_dict = {"insert_me":"Hello! I am from views.py"}
-    messages.success(request, "Welcome to our Bishok Multimedia")
+    messages.success(request, "Welcome to Bishok Multimedia")
     
    # return HttpResponse("this is home page")
     return render(request,'index.html', my_dict)
     
 def about(request):
     # Retrieve all news objects
-    news_list = News.objects.all()
+    about_list = News.objects.all()
 
     # Pass the news_list to the template
-    return render(request, 'about.html', {'news_list': news_list})
+    return render(request, 'about.html', {'about_list': about_list})
     #return HttpResponse("this is about page")
     
 
@@ -41,5 +77,12 @@ def contact(request):
     return render(request,'contact.html')
 
 def service(request):
+    videos = Video.objects.all()
     #return HttpResponse("this is service page")
-    return render(request,'service.html')
+    return render(request,'service.html',{'videos': videos})
+
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged Out successfully!")
+    return render(request,'signin.html')
